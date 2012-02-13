695f312978b3d208b0bc95fcbf5c65c4786cedd5
Joshua Priddle <jpriddle@me.com>
2012-02-13 01:54:00 -0500

    tweak installer

--- vim/share/install-vim.sh
+++ vim/share/install-vim.sh
@@ -5,9 +5,13 @@ tar -vjxf vim-7.3.tar.bz2
 cd vim73
 ./configure --prefix="$HOME/local" \
             --with-features=huge \
+            --enable-gui=no \
+            --with-tlib=ncurses \
+            --disable-nls \
             --enable-multibyte \
             --enable-perlinterp \
             --enable-pythoninterp \
             --enable-rubyinterp
+
 make
 make install
