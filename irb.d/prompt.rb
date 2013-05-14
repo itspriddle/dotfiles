f9a60da8c2b1ac604ad7c8799484d43681befd28
Joshua Priddle <jpriddle@me.com>
2013-05-14 12:11:08 -0400

    Fix irb prompt on older rubies

--- irb.d/prompt.rb
+++ irb.d/prompt.rb
@@ -4,7 +4,8 @@ IRB.conf[:SAVE_HISTORY] = 2000
 IRB.conf[:HISTORY_FILE] = "#{ENV['HOME']}/.irb_history"
 IRB.conf[:AUTO_INDENT]  = true
 
-prompt  = "#{RUBY_VERSION}-p#{RUBY_PATCHLEVEL} "
+prompt = RUBY_VERSION
+prompt << "-p#{RUBY_PATCHLEVEL}" if defined? RUBY_PATCHLEVEL
 
 if defined? Rails
   project = `git rev-parse --show-toplevel 2> /dev/null`.strip
