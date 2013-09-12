345cb00f6c85c43b0785ea7d9be91171a99fcb84
Joshua Priddle <jpriddle@me.com>
2013-09-12 10:07:36 -0400

    File.expand_path already works on PWD

--- irb.d/localrc.rb
+++ irb.d/localrc.rb
@@ -1,5 +1,5 @@
 # Load local irbrc file if it exists (and is not this file)
-localrc = File.expand_path(File.join(Dir.pwd, ".irbrc"))
+localrc = File.expand_path(".irbrc")
 if File.exists?(localrc) && localrc != File.expand_path(__FILE__)
   load localrc
 end
