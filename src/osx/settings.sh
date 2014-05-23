9ec87464b20d9de37a98647f367d2a0f3bd64991
Joshua Priddle <jpriddle@me.com>
2014-05-23 14:41:46 -0400

    Add some OS X settings

---
+++ src/osx/settings.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Save to disk (not to iCloud) by default
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+
+# Reveal IP address, hostname, OS version, etc. when clicking the clock
+# in the login window
+defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
+
+# Check for software updates daily, not just once per week
+defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
+
+# Trackpad: enable tap to click for this user and for the login screen
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+# Use scroll gesture with the Ctrl (^) modifier key to zoom
+defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool true
+defaults write com.apple.universalaccess HIDScrollZoomModifierMask -int 262144
+
+## Safari
+
+# Show status bar
+defaults write com.apple.Safari 'ShowStatusBar' -bool true
+
+# Enable dev menu
+defaults write com.apple.Safari 'IncludeDevelopMenu' -bool true
+
+# MacVim: Zoom both ways when clicking + (maximize) icon
+defaults write org.vim.MacVim MMZoomBoth 1
