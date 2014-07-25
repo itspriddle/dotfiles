27440064b8dad87650991769b906c12239d00e53
Joshua Priddle <jpriddle@me.com>
2014-07-25 18:45:57 -0400

    Brewfile is deprecated :(

---
+++ src/osx/install-brews.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+brew tap itspriddle/homebrew-site5
+brew install brew-pip
+brew install ctags
+brew install gh
+brew install gist
+brew install git
+brew install gnupg
+brew install imagemagick
+brew install macvim --override-system-vim
+brew install mysql
+brew install node
+brew install phantomjs
+brew install phpunit
+brew install pv
+brew install rbenv
+brew install redis
+brew install rename
+brew install ruby-build
+brew install the_silver_searcher
+brew install tree
+brew install vnstat
+brew install wget
+brew install zone2sql
