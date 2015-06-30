c023bc5c0a29905c7f0ab966e312c2d2530e1240
Joshua Priddle <jpriddle@me.com>
2015-06-30 10:52:04 -0400

    Update bash_profile

--- bash_profile
+++ bash_profile
@@ -1,18 +1,4 @@
-export CLICOLOR=1
-export TERM='xterm-256color'
-export EDITOR=vim
-export GIT_EDITOR="$EDITOR "
-
-new_paths=(
-  $HOME/.dotfiles/bin
-)
-
-for p in "${new_paths[@]}"
-do
-  [[ -d $p ]] && export PATH="$p:$PATH"
-done
-
-unset new_paths
+source ~/.dotfiles/profile.d/init.sh
 
 source $HOME/.bashrc
 
---
+++ profile.d/env.sh
@@ -0,0 +1,23 @@
+export CLICOLOR=1
+export EDITOR=vim
+export GIT_EDITOR="$EDITOR "
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export TERM="xterm-256color"
+
+PATH="${HOME}/.dotfiles/bin:/usr/local/bin:/usr/local/sbin:/usr/local/share/npm/bin:/usr/sbin:/sbin:/usr/bin:/bin"
+
+if [ "${OSTYPE:0:6}" = darwin ]; then
+  # Disable emoji when installing packages with Homebrew
+  export HOMEBREW_NO_EMOJI=1
+
+  # $(sysctl -n hw.ncpu) + 1
+  export MAKEFLAGS="-j9"
+
+  PATH="/Applications/Postgres.app/Contents/Versions/9.4/bin:${PATH}"
+fi
+
+export PATH
+
+# Clear PROMPT_COMMAND, avoids "command not found" errors when using `sh`.
+unset PROMPT_COMMAND
