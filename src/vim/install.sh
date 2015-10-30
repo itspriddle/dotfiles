3ae4689e4b4c42607903c73201313ba66e28a3a6
Joshua Priddle <jpriddle@me.com>
2015-10-30 16:42:56 -0400

    vim: Move install script, remove csstidy.tpl

--- src/vim/install.sh
+++ src/vim/install.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env bash
+#!/usr/bin/sh
 
-wget ftp://ftp.vim.org/pub/vim/unix/vim-7.3.tar.bz2
-tar -vjxf vim-7.3.tar.bz2
-cd vim73
-./configure --prefix="${PREFIX:-$HOME/local}" \
+wget ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2
+tar -vjxf vim-7.4.tar.bz2
+cd vim74
+./configure --prefix="$HOME/local" \
             --with-features=huge \
             --enable-gui=no \
             --with-tlib=ncurses \
