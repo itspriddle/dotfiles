22f02773deeac4ff51d08f0ff5225458d6d7b84d
Joshua Priddle <jpriddle@me.com>
2015-12-02 00:09:43 -0500

    shell: Remove VISCOSITY_DEFAULT_VPN env var

--- profile.d/env.sh
+++ profile.d/env.sh
@@ -51,6 +51,3 @@ if [ "${OSTYPE:0:6}" = "darwin" ]; then
 elif [ "${OSTYPE:0:5}" = "linux" ]; then
   export LS_COLORS="di=1;34;27:ln=1;36;27:so=1;35;27:pi=33;27:ex=1;32;27:bd=1;33;27:cd=1;33;27:su=1;31;27:sg=1;31;27:tw=1;34;27:ow=1;34;27:"
 fi
-
-# Viscosity
-export VISCOSITY_DEFAULT_VPN="Helix VPN"
