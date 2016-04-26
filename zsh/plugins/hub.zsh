7296d31a003d9504e6d23e3fce0b7ac04ed72f47
Joshua Priddle <jpriddle@me.com>
2016-04-26 15:07:06 -0400

    zsh: Remove completion for `git repl`

--- zsh/plugins/hub.zsh
+++ zsh/plugins/hub.zsh
@@ -2,7 +2,6 @@ source ~/.dotfiles/profile.d/hub.sh
 
 # Custom completion for git
 zstyle ':completion:*:*:hub:*' user-commands \
-  repl:'Open a repl' \
   pr:'Push branch to GitHub and open a Pull request' \
   count:'Show commit count by author'
 
