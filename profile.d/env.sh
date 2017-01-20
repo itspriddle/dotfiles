1d71e6335963da7bfa48039302811ff020be69fa
Joshua Priddle <jpriddle@me.com>
2017-01-20 14:49:21 -0500

    env: Set FZF colors, default command

--- profile.d/env.sh
+++ profile.d/env.sh
@@ -57,6 +57,10 @@ elif [ "${OSTYPE:0:5}" = "linux" ]; then
   export LS_COLORS="di=1;34;27:ln=1;36;27:so=1;35;27:pi=33;27:ex=1;32;27:bd=1;33;27:cd=1;33;27:su=1;31;27:sg=1;31;27:tw=1;34;27:ow=1;34;27:"
 fi
 
+# FZF
+export FZF_DEFAULT_OPTS="--color fg:-1,bg:-1,hl:33,fg+:4,bg+:-1,hl+:32,prompt:-1"
+export FZF_DEFAULT_COMMAND="ag -g ''"
+
 # Private ENV vars, this file is ignored by git
 if [ -f "$(dirname "$0")/env-private.sh" ]; then
   source "$(dirname "$0")/env-private.sh"
