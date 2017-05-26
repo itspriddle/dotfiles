d117d959803014b58c43b87842a842c4293121f8
Joshua Priddle <jpriddle@me.com>
2017-05-25 23:47:32 -0400

    zsh: Remove completion for `git pr`

--- zsh/plugins/hub.zsh
+++ zsh/plugins/hub.zsh
@@ -2,7 +2,6 @@ source ~/.dotfiles/profile.d/hub.sh
 
 # Custom completion for git
 zstyle ':completion:*:*:hub:*' user-commands \
-  pr:'Push branch to GitHub and open a Pull request' \
   count:'Show commit count by author'
 
 # Completion `hub` and `git`
