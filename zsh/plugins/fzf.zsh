106978dd4d130e940c358f2019de0ee7d9f31bba
Joshua Priddle <jpriddle@me.com>
2017-06-14 17:39:27 -0400

    zsh: Load fzf keybindings
    But not completion, not sure I like it.

---
+++ zsh/plugins/fzf.zsh
@@ -0,0 +1,5 @@
+[ -r /usr/local/opt/fzf/shell/key-bindings.zsh ] &&
+  source /usr/local/opt/fzf/shell/key-bindings.zsh
+
+# [ -r /usr/local/opt/fzf/shell/completion.zsh ] &&
+#   source /usr/local/opt/fzf/shell/completion.zsh
