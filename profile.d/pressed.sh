3bfce897eacde6750a4adb791566370ce0ad0c1c
Joshua Priddle <jpriddle@me.com>
2017-06-14 17:42:14 -0400

    env: Update Helix shell helpers

--- profile.d/pressed.sh
+++ profile.d/pressed.sh
@@ -1,12 +1,12 @@
 # Pressed Helix apps, disable progress bar rspec formatter
 export DISABLE_HELIX_CORE_RSPEC_FORMATTER=1
 
-alias helix-console="~/work/helix/bin/rails console"
-alias helix-core-console="~/work/helix_core/script/console"
-alias backstage-console="~/work/backstage/bin/rails console"
-alias eventhorizon-console="~/work/event_horizon/bin/rails console"
-
-alias hcc=helix-core-console
+# Open a helix_core console
+hcc() {
+  cd ~/work/helix_core
+  ./script/console
+  cd -
+}
 
 # Setup pressed
 command -v pressed > /dev/null && eval "$(pressed init -)"
