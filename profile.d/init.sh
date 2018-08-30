7cd500ff96a2e46508132e8d325bdbff13c8d1c1
Joshua Priddle <jpriddle@me.com>
2018-08-29 23:17:01 -0400

    env: avoid sub shell in init.sh

--- profile.d/init.sh
+++ profile.d/init.sh
@@ -1,7 +1,7 @@
 if [ -d ~/.dotfiles/profile.d ]; then
   for i in ~/.dotfiles/profile.d/*.sh; do
-    if [ -r $i ] && [ "$(basename "$i")" != "init.sh" ]; then
-      source $i
+    if [ -r "${i}" ] && [ "${i##*/}" != "init.sh" ]; then
+      source "${i}"
     fi
   done
   unset i
