15bb5d2ae0418e110134195dae75d8972de889f8
Joshua Priddle <jpriddle@me.com>
2018-09-03 23:58:38 -0400

    bash: Simplify git alias completion

---
+++ bash/completion.d/git.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Based on https://gist.github.com/JuggoPop/10706934
+
+# Allow aliases to use completion
+__git_complete gb  _git_branch
+__git_complete gbd _git_branch
+__git_complete gco _git_checkout
+__git_complete gd  _git_diff
+__git_complete gdc _git_diff
--- bashrc
+++ bashrc
@@ -3,7 +3,7 @@
 # If not running interactively, don't do anything
 [ -z "$PS1" ] && return
 
+source ~/.dotfiles/zsh/aliases
 source ~/.dotfiles/bash/config
 source ~/.dotfiles/bash/completion
 source ~/.dotfiles/bash/prompt
-source ~/.dotfiles/zsh/aliases
