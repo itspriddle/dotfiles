66179e35a9d38f385c57703f6286473e3a6faf03
Joshua Priddle <jpriddle@me.com>
2021-12-18 20:00:58 -0500

    bash: Check for required function for git completion

--- bash/completion.d/git.sh
+++ bash/completion.d/git.sh
@@ -2,9 +2,11 @@
 
 # Based on https://gist.github.com/JuggoPop/10706934
 
-# Allow aliases to use completion
-__git_complete gb  _git_branch
-__git_complete gbd _git_branch
-__git_complete gco _git_checkout
-__git_complete gd  _git_diff
-__git_complete gdc _git_diff
+if type __git_complete &> /dev/null; then
+  # Allow aliases to use completion
+  __git_complete gb  _git_branch
+  __git_complete gbd _git_branch
+  __git_complete gco _git_checkout
+  __git_complete gd  _git_diff
+  __git_complete gdc _git_diff
+fi
