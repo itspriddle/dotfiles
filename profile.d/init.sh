daa9b73ea0aebf0095280fbda5fa95fa80050bf4
Joshua Priddle <jpriddle@me.com>
2022-08-18 22:04:45 -0400

    env: remove unused gpg, init files

--- profile.d/init.sh
+++
@@ -1,8 +0,0 @@
-if [ -d ~/.dotfiles/profile.d ]; then
-  for i in ~/.dotfiles/profile.d/*.sh; do
-    if [ -r "${i}" ] && [ "${i##*/}" != "init.sh" ]; then
-      source "${i}"
-    fi
-  done
-  unset i
-fi
